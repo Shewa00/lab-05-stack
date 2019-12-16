078c7a0d43d857ddd4b854c6e7a20b3b1e16f36c
ARAOvsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2019-12-16 15:53:31 +0300

    Update task2.hpp

--- include/task2.hpp
+++ include/task2.hpp
@@ -11,16 +11,17 @@ public:
     template <typename ... Args>
     void push_emplace(Args&& ... value) {
         auto node = Stack<T>::head;
-        Stack<T>::head = new typename Stack<T>::Node{std::forward<Args>(value)...,node};
+        Stack<T>::head = new typename
+                Stack<T>::Node{std::forward<Args>(value)..., node};
     }
 
     T pop() {
-        if(Stack<T>::head) {
+        if (Stack<T>::head) {
             auto node = Stack<T>::head->data;
             Stack<T>::head = Stack<T>::head->next;
             return node;
-        }
-        else
+        } else {
             throw std::exception();
+        }
     }
 };

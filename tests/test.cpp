420dbc6792b692080d5f63b1c02d6653b96e5b9a
ARAOvsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2019-12-16 15:52:25 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -4,16 +4,16 @@
 #include <task1.hpp>
 #include <task2.hpp>
 
-TEST(Stack,push_test_1)
+TEST(Stack, push_test_1)
 {
     Stack<int> my_stack{};
     for (size_t index = 1; index <= 10; ++index) {
         my_stack.push(index);
     }
-    EXPECT_EQ(my_stack.Head(),10);
+    EXPECT_EQ(my_stack.Head(), 10);
 }
 
-TEST(Stack,push_test_2)
+TEST(Stack, push_test_2)
 {
     Stack<int> my_stack{};
 
@@ -21,22 +21,22 @@ TEST(Stack,push_test_2)
     my_stack.push(5);
     my_stack.push(10);
 
-    EXPECT_EQ(my_stack.Head(),10);
+    EXPECT_EQ(my_stack.Head(), 10);
 }
 
-TEST(Stack,push_test_3)
+TEST(Stack, push_test_3)
 {
     Stack<int> my_stack{};
 
     my_stack.push(1);
-    EXPECT_EQ(my_stack.Head(),1);
+    EXPECT_EQ(my_stack.Head(), 1);
     my_stack.push(5);
-    EXPECT_EQ(my_stack.Head(),5);
+    EXPECT_EQ(my_stack.Head(), 5);
     my_stack.push(10);
-    EXPECT_EQ(my_stack.Head(),10);
+    EXPECT_EQ(my_stack.Head(), 10);
 }
 
-TEST(Stack,pop_test_1)
+TEST(Stack, pop_test_1)
 {
     F_Stack<int> my_stack{};
 
@@ -44,7 +44,7 @@ TEST(Stack,pop_test_1)
     my_stack.push(5);
     my_stack.pop();
 
-    EXPECT_EQ(my_stack.Head(),1);
+    EXPECT_EQ(my_stack.Head(), 1);
 }
 
 TEST(Stack, push_emplace)
@@ -52,20 +52,20 @@ TEST(Stack, push_emplace)
     S_Stack<int> my_stack{};
 
     my_stack.push_emplace(1);
-    EXPECT_EQ(my_stack.Head(),1);
+    EXPECT_EQ(my_stack.Head(), 1);
     my_stack.push_emplace(5);
-    EXPECT_EQ(my_stack.Head(),5);
+    EXPECT_EQ(my_stack.Head(), 5);
     my_stack.push_emplace(10);
-    EXPECT_EQ(my_stack.Head(),10);
+    EXPECT_EQ(my_stack.Head(), 10);
 }
 
-TEST(Stack, pop_test){
-
+TEST(Stack, pop_test)
+{
     S_Stack<int> my_stack{};
 
     my_stack.push(1);
     my_stack.push(5);
     my_stack.pop();
 
-    EXPECT_EQ(my_stack.Head(),1);
+    EXPECT_EQ(my_stack.Head(), 1);
 }
